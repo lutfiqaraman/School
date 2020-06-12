a64135b1f6e2ad2207904ac2c849d6adf81cbd2f
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-12 11:16:32 +0300

    edit teacher repo

--- backend/Data/TeacherRepository.cs
+++ backend/Data/TeacherRepository.cs
@@ -8,6 +8,12 @@ namespace backend.Data
   {
     private readonly IConfiguration _config;
 
+    // To get all teachers from database
+    public Task<Teacher> GetAllTeachers()
+    {
+      throw new System.NotImplementedException();
+    }
+
     public TeacherRepository(IConfiguration config)
     {
       _config = config;
@@ -24,11 +30,6 @@ namespace backend.Data
     }
 
     public Task<Teacher> GetTeacher()
-    {
-      throw new System.NotImplementedException();
-    }
-
-    public Task<Teacher> GetAllTeachers()
     {
       throw new System.NotImplementedException();
     }
