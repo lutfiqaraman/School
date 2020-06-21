4dd16e37cf731a8541fa90f5e031cdf8abfd5959
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-21 22:16:55 +0300

    import teacher model

--- frontend/src/app/services/teacher.service.ts
+++ frontend/src/app/services/teacher.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { ITeacher } from '../models/ITeacher';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
+  private teacherList: ITeacher[] = [];
 
   constructor() { }
 }
