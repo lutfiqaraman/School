12759121bb7f164ec6f1046b6cf96430fb3d249b
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-22 22:46:30 +0300

    get a teacher service

--- frontend/src/app/components/teacher/teacher.component.ts
+++ frontend/src/app/components/teacher/teacher.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TeacherService } from 'src/app/services/teacher.service';
+import { ITeacher } from 'src/app/models/ITeacher';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-teacher',
@@ -9,15 +11,15 @@ import { TeacherService } from 'src/app/services/teacher.service';
 export class TeacherComponent implements OnInit {
 
   constructor(public teacherService: TeacherService) { }
-
+  teacher: ITeacher[] = [];
   ngOnInit(): void {
     this.getTeacher();
   }
 
-  getTeacher(): void {
-    const id = '1';
+  getTeacher() {
+    const id = '2';
     this.teacherService.getTeacherById(id).subscribe((data) => {
-      console.log(data);
+      return data;
     });
   }
 
--- frontend/src/app/services/teacher.service.ts
+++ frontend/src/app/services/teacher.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { ITeacher } from '../models/ITeacher';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,10 @@ export class TeacherService {
   constructor(private http: HttpClient) {}
 
   // Teacher - Get a teacher by Id
-  getTeacherById(teacherId: string)
+  getTeacherById(teacherId: string): Observable<ITeacher[]>
   {
     this.url = 'http://localhost:5000/teachers/getteacher/' + teacherId;
-    return this.http.get<ITeacher>(this.url);
+    return this.http.get<ITeacher[]>(this.url);
   }
 
 }
