0b48901684a32093eb9b570e41c0d43742a94c68
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-23 19:16:31 +0300

    display teacher data

--- frontend/src/app/components/teacher/teacher.component.ts
+++ frontend/src/app/components/teacher/teacher.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TeacherService } from 'src/app/services/teacher.service';
 import { ITeacher } from 'src/app/models/ITeacher';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-teacher',
@@ -9,17 +9,19 @@ import { Observable } from 'rxjs';
   styleUrls: ['./teacher.component.css']
 })
 export class TeacherComponent implements OnInit {
-
-  constructor(public teacherService: TeacherService) { }
   teacher: ITeacher[] = [];
+  datasource: Observable<ITeacher[]>;
+
+  constructor(public teacherService: TeacherService) {}
+
   ngOnInit(): void {
     this.getTeacher();
   }
 
   getTeacher() {
-    const id = '2';
-    this.teacherService.getTeacherById(id).subscribe((data) => {
-      return data;
+    const id = 1;
+    return this.teacherService.getTeacherById(id).subscribe((data) => {
+      this.teacher = data;
     });
   }
 
--- frontend/src/app/services/teacher.service.ts
+++ frontend/src/app/services/teacher.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { ITeacher } from '../models/ITeacher';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
-  private teacherList: ITeacher[] = [];
   private url: string;
 
   constructor(private http: HttpClient) {}
 
   // Teacher - Get a teacher by Id
-  getTeacherById(teacherId: string): Observable<ITeacher[]>
+  getTeacherById(teacherId: number): Observable<ITeacher[]>
   {
     this.url = 'http://localhost:5000/teachers/getteacher/' + teacherId;
     return this.http.get<ITeacher[]>(this.url);
