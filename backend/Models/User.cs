dd35d9c0696e5e5faa7a0e4981625248f07eea42
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-29 21:44:28 +0300

    Update user model

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,9 +1,10 @@
 namespace backend.Models
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-    }
+  public class User
+  {
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+  }
 }
