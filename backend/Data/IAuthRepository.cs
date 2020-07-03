fbfe15271e1d6459b312c808cf1c35a4c26324cc
lutfiqaraman <lutfiqaraman@gmail.com>
2020-07-03 09:57:50 +0300

    Login method

--- backend/Data/IAuthRepository.cs
+++ backend/Data/IAuthRepository.cs
@@ -5,8 +5,8 @@ namespace backend.Data
 {
   public interface IAuthRepository
   {
-    User Register(User user, string password);
-    User Login(string username, string password);
+    Task<User> Register(User user, string password);
+    Task<User> Login(string username, string password);
     Task<bool> IsUserExist(string username);
   }
 }
