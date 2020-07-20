246378ec80c038e9644b930374d37f1dcf7a0edd
lutfiqaraman <lutfiqaraman@gmail.com>
2020-07-20 20:50:18 +0300

    add register form function

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register()
+  {
+    console.log(this.model);
+  }
+
+  cancel()
+  {
+    console.log('click on cancel');
   }
 
 }
